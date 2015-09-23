b7db163ff9e9aaeec7c316d420ecbe8f79082c0e
Petr Penzin <penzin.dev@gmail.com>
2015-09-22 20:38:08 -0700

    Initial commit. Some tests are passing, but can't disassemble helloworld yet. No docs either.

---
+++ src/lib/Neko.hs
@@ -0,0 +1,173 @@
+module Neko where
+
+import Data.ByteString.Lazy as BS
+import Data.ByteString.Lazy.Char8 as BSChar
+import Data.Maybe
+import Data.Either
+import Data.Word
+import Data.Int
+
+import Neko.IO
+
+data Instruction = 
+        -- getters
+        AccNull
+      | AccTrue
+      | AccFalse
+      | AccThis
+      | AccInt Int
+      | AccStack Int
+      | AccGlobal Int
+      | AccEnv Int
+      | AccField String
+      | AccArray
+      | AccIndex Int
+      | AccBuiltin String
+        -- setters
+      | SetStack Int
+      | SetGlobal Int
+      | SetEnv Int
+      | SetField String
+      | SetArray
+      | SetIndex Int
+      | SetThis
+        -- stack ops
+      | Push
+      | Pop Int
+      | Call Int
+      | ObjCall Int
+      | Jump Int
+      | JumpIf Int
+      | JumpIfNot Int
+      | Trap Int
+      | EndTrap
+      | Ret Int
+      | MakeEnv Int
+      | MakeArray Int
+        -- value ops
+      | Bool
+      | IsNull
+      | IsNotNull
+      | Add
+      | Sub
+      | Mult
+      | Div
+      | Mod
+      | Shl
+      | Shr
+      | UShr
+      | Or
+      | And
+      | Xor
+      | Eq
+      | Neq
+      | Gt
+      | Gte
+      | Lt
+      | Lte
+      | Not
+        -- extra ops
+      | TypeOf
+      | Compare
+      | Hash
+      | New
+      | JumpTable Int
+      | Apply Int
+      | AccStack0
+      | AccStack1
+      | AccIndex0
+      | AccIndex1
+      | PhysCompare
+      | TailCall (Int, Int)
+      | Loop
+      deriving (Show, Eq)
+
+data Global =
+        GlobalVar String
+      | GlobalFunction (Int, Int)
+      | GlobalString String
+      | GlobalFloat String
+      | GlobalDebug ([String], [(Int, Int)])
+      | GlobalVersion Int
+      deriving (Show, Eq)
+
+data Module = N {globals::[Global], code::[Instruction]} deriving (Show, Eq)
+
+-- Return module or return an error
+readModule :: ByteString -> Either String Module
+readModule bs = if (isNothing afterMagic) then (Left "Failed to read magic value")
+                else readModuleData $ fromJust afterMagic
+    where afterMagic = stripMagic bs
+
+-- Process module without magic value
+readModuleData :: ByteString -> Either String Module
+readModuleData bs = if (isNothing moduleFields) then (Left err) else Right N {globals=[], code=[]}
+    where (rest, err, moduleFields) = readModuleFields bs
+
+-- Read fields that determine code size, number of globals, etc
+readModuleFields :: ByteString -> (ByteString, String, Maybe (Int32, Int32, Int32))
+readModuleFields bs = if (isNothing resNumGlobals) then (bs, errNumGlobals, Nothing) else
+                      if (isNothing resNumFields) then (bs, errNumFields, Nothing) else
+                      if (isNothing resCodeSize) then (bs, errCodeSize, Nothing) else
+                      if (isJust checkError) then (bs, fromJust checkError, Nothing) else
+                      (bsAfter, "Success", Just (numGlobals, numFields, codeSize))
+                    where
+                      resNumGlobals = readInt32 bs
+                      (numGlobals, afterNumGlobals) = fromJust resNumGlobals
+                      resNumFields = readInt32 afterNumGlobals
+                      (numFields, afterNumFields) = fromJust resNumFields
+                      resCodeSize = readInt32 afterNumFields
+                      (codeSize, bsAfter) = fromJust resCodeSize
+                      errNumGlobals = "Failed to read number of globals"
+                      errNumFields = "Failed to read number of fields"
+                      errCodeSize = "Failed to read code size"
+                      checkError = checkModuleFields numGlobals numFields codeSize
+
+-- Check that module fields have valid values
+checkModuleFields :: Int32 -> Int32 -> Int32 -> Maybe String
+checkModuleFields globals fields code
+     = if (globals < 0 || globals > 0xFFFF) then Just "Number of globals not between 0 and 0xFFFF" else
+       if (fields < 0 || fields > 0xFFFF) then Just "Number of fields not between 0 and 0xFFFF" else
+       if (code < 0 || code > 0xFFFFFF) then Just "Code size not between 0 and 0xFFFFFF" else
+       Nothing
+
+-- Read globals
+readGlobals :: Integer -> ByteString -> Maybe ([Global], ByteString)
+readGlobals 0 bs = Just ([], bs)
+readGlobals n bs = if (isNothing current) then Nothing else res
+    where current = readGlobal bs
+          (val, rest) = fromJust current
+          next = readGlobals (n - 1) rest
+          res = if (isNothing next) then Nothing else Just ((val:vals), theTail)
+          (vals, theTail) = fromJust next
+
+-- Read fields
+
+-- Read instructions
+
+-- Read a single global value, if succesfull return a single global value and remaining bytestring
+readGlobal :: ByteString -> Maybe (Global, ByteString)
+readGlobal bs = if (isPrefixOf (BS.singleton 1) bs) then Just (GlobalVar "TODO", empty) else -- TODO
+                if (isPrefixOf (BS.singleton 2) bs) then Just (GlobalFunction (0, 0), empty) else -- TODO
+                if (isPrefixOf (BS.singleton 3) bs) then (readGlobalString rest) else
+                if (isPrefixOf (BS.singleton 4) bs) then Just (GlobalFloat "TODO", empty) else -- TODO
+                if (isPrefixOf (BS.singleton 5) bs) then Just (GlobalDebug (["TODO"], []), empty) else -- TODO
+                if (isPrefixOf (BS.singleton 6) bs) then Just (GlobalVersion 0, empty) else -- TODO
+                Nothing
+    where rest = BS.drop 1 bs
+
+-- Read global string
+readGlobalString :: ByteString -> Maybe (Global, ByteString)
+readGlobalString bs = if (isNothing maybeLength) then Nothing else
+                      if (BS.length subByteString < length) then Nothing else
+                      Just (GlobalString (BSChar.unpack subByteString), ret)
+    where maybeLength = readUInt16 bs
+          (len, rest) = fromJust maybeLength
+          length = (fromIntegral len)::Int64
+          subByteString = BS.take length rest
+          ret = BS.drop length rest
+
+-- Check first four bytes for magic value. Return the rest of the string if OK, otherwise return nothing
+stripMagic :: ByteString -> Maybe ByteString
+stripMagic bs = if (isPrefixOf (BSChar.pack "NEKO") bs) then (Just $ BS.drop 4 bs) else Nothing
+
---
+++ src/lib/Neko/IO.hs
@@ -0,0 +1,35 @@
+module Neko.IO where
+
+import Data.ByteString.Lazy as BS
+import Data.Maybe
+import Data.Bits
+import Data.Word
+import Data.Int
+
+-- Read an arbitrary-sized integer, Neko style. Size is in bytes. Do we need to hande endianness?
+readVarInt :: Int64 -> ByteString -> Maybe (Integer, ByteString)
+readVarInt size bs = if (BS.length chunk < size) then Nothing else Just (fuse 0 (BS.unpack chunk), rest)
+    where chunk = BS.take size bs
+          rest = BS.drop size bs
+          fuse :: Integer -> [Word8] -> Integer
+          fuse _ [] = 0
+          fuse pos (w:ws) = ((fromIntegral w) `shiftL` (fromIntegral $ pos * 8)) + fuse (pos + 1) ws
+
+-- Read an arbitrary-sized unsigned integer, Neko style. Size is in bytes.
+readVarUInt :: Int64 -> ByteString -> Maybe (Word, ByteString)
+readVarUInt size bs = if (isNothing signed) then Nothing else Just ((fromInteger num), rest)
+    where signed = readVarInt size bs
+          (num, rest) = fromJust signed
+
+-- Read a 32-bit integer, Neko style.
+readInt32 :: ByteString -> Maybe (Int32, ByteString)
+readInt32 bs = if (isNothing r) then Nothing else Just (fromIntegral num, rest)
+    where r = readVarInt 4 bs
+          (num, rest) = fromJust r
+
+-- Read a 16-bit unsigned integer, Neko style.
+readUInt16 :: ByteString -> Maybe (Word16, ByteString)
+readUInt16 bs = if (isNothing r) then Nothing else Just (fromIntegral num, rest)
+    where r = readVarUInt 2 bs
+          (num, rest) = fromJust r
+
